e217fef94df1de9cb6b9675941b6210141b0c5dd
Wicktorl <wicktor90@hotmail.com>
2018-09-07 11:28:40 +0200

    Lab1

--- Lab_1/Lab1_LED/Lab1/src/main.c
+++ Lab_1/Lab1_LED/Lab1/src/main.c
@@ -15,6 +15,10 @@
 #define LED2_PIN ( LED2_GPIO & ( GPIO_MAX_PIN_NUMBER -1))
 #define LED2_BIT_VALUE (1 << LED2_PIN )
 
+#define LED3_PORT ( LED3_GPIO / GPIO_MAX_PIN_NUMBER )
+#define LED3_PIN ( LED3_GPIO & ( GPIO_MAX_PIN_NUMBER -1))
+#define LED3_BIT_VALUE (1 << LED3_PIN )
+
 #define BUTTON_PORT0 (GPIO_PUSH_BUTTON_0 >> 5)
 #define BUTTON_PIN0 (1 << (GPIO_PUSH_BUTTON_0 & 0x1f))
 
@@ -30,13 +34,17 @@ void mdelay(int ms){
 		cycles--;
 	}
 }
-
-void initButton(void){
+void initButton(void)
+{
+	AVR32_GPIO.port[BUTTON_PORT0].gpers = BUTTON_PIN0;
+	AVR32_GPIO.port[BUTTON_PORT0].oderc = BUTTON_PIN0;
 	
+	AVR32_GPIO.port[BUTTON_PORT1].gpers = BUTTON_PIN1;
+	AVR32_GPIO.port[BUTTON_PORT1].oderc = BUTTON_PIN1;
 	
-	
+	AVR32_GPIO.port[BUTTON_PORT2].gpers = BUTTON_PIN2;
+	AVR32_GPIO.port[BUTTON_PORT2].oderc = BUTTON_PIN2;
 }
-
 void initLED (void)
 {
 	volatile avr32_gpio_port_t * led0_port;
@@ -56,49 +64,34 @@ void initLED (void)
 	led2_port->gpers = LED2_BIT_VALUE;
 	led2_port->ovrs = LED2_BIT_VALUE;
 	led2_port->oders = LED2_BIT_VALUE;
+	
+	volatile avr32_gpio_port_t * led3_port;
+	led3_port = &AVR32_GPIO.port[LED2_PORT];
+	led3_port->gpers = LED2_BIT_VALUE;
+	led3_port->ovrs = LED2_BIT_VALUE;
+	led3_port->oders = LED2_BIT_VALUE;
 }
-
-void initBTN (void)
-{
-    volatile int button_state0;
-    volatile avr32_gpio_port_t * button_port0;
-    button_port0 = &AVR32_GPIO.port[BUTTON_PORT0];
-    
-    volatile int button_state1;
-    volatile avr32_gpio_port_t * button_port1;
-    button_port0 = &AVR32_GPIO.port[BUTTON_PORT1];
-    
-    volatile int button_state2;
-    volatile avr32_gpio_port_t * button_port2;
-    button_port0 = &AVR32_GPIO.port[BUTTON_PORT2];
-    
-    button_port0 = &AVR32_GPIO.port[BUTTON_PORT0];
-    button_port0->gpers = BUTTON_PIN0;
-    button_port0->oderc = BUTTON_PIN0;
-    button_port1 = &AVR32_GPIO.port[BUTTON_PORT1];
-    button_port1->gpers = BUTTON_PIN1;
-    button_port1->oderc = BUTTON_PIN1;
-    button_port2 = &AVR32_GPIO.port[BUTTON_PORT2];
-    button_port2->gpers = BUTTON_PIN2;
-    button_port2->oderc = BUTTON_PIN2;
-}
-
 int main (void)
 {
 	
 	initLED ();
-    initBTN ();
+	//initButton();
+
+	volatile int button_state0;
+	volatile int button_state1;
+	volatile int button_state2;
+	int count = 0;
 	
 	while(1) {
-		button_state0 = button_port0->pvr & BUTTON_PIN0;
-		button_state1 = button_port1->pvr & BUTTON_PIN1;
-		button_state2 = button_port2->pvr & BUTTON_PIN2;
+		button_state0 = AVR32_GPIO.port[BUTTON_PORT0].pvr & BUTTON_PIN0;
+		button_state1 = AVR32_GPIO.port[BUTTON_PORT1].pvr & BUTTON_PIN1;
+		button_state2 = AVR32_GPIO.port[BUTTON_PORT2].pvr & BUTTON_PIN2;
 		
 		//Event 1 - Blinking LED1
 		if(!button_state0){
 			for(int i = 0; i <= 10; i++){
 				AVR32_GPIO.port[LED0_PORT].ovrt = LED0_BIT_VALUE;
-				mdelay(300);
+				mdelay(100);
 			}
 			AVR32_GPIO.port[LED0_PORT].ovrs = LED0_BIT_VALUE;
 		}
@@ -106,9 +99,10 @@ int main (void)
 		//Event 2 - Light LED2 when pushed
 		if(!button_state1){
 			AVR32_GPIO.port[LED1_PORT].ovrt = LED1_BIT_VALUE;
+			count++;
 			
-			while(!button_state1){	
-				button_state1 = button_port1->pvr & BUTTON_PIN1;
+			while(!button_state1){
+				button_state1 = AVR32_GPIO.port[BUTTON_PORT1].pvr & BUTTON_PIN1;
 			}
 			AVR32_GPIO.port[LED1_PORT].ovrt = LED1_BIT_VALUE;
 		}
@@ -118,9 +112,36 @@ int main (void)
 			AVR32_GPIO.port[LED2_PORT].ovrt = LED2_BIT_VALUE;
 			
 			while(!button_state2){
-				button_state2 = button_port2->pvr & BUTTON_PIN2;
+				button_state2 = AVR32_GPIO.port[BUTTON_PORT2].pvr & BUTTON_PIN2;
 			}
 		}
 		
+		if(count == 3){
+			
+			AVR32_GPIO.port[LED0_PORT].ovrt = LED0_BIT_VALUE;
+			mdelay(500);
+			AVR32_GPIO.port[LED0_PORT].ovrt = LED0_BIT_VALUE;
+			AVR32_GPIO.port[LED1_PORT].ovrt = LED1_BIT_VALUE;
+			mdelay(500);
+			AVR32_GPIO.port[LED1_PORT].ovrt = LED1_BIT_VALUE;
+			AVR32_GPIO.port[LED2_PORT].ovrt = LED2_BIT_VALUE;
+			mdelay(500);
+			AVR32_GPIO.port[LED2_PORT].ovrt = LED2_BIT_VALUE;
+			AVR32_GPIO.port[LED3_PORT].ovrt = LED3_BIT_VALUE;
+
+			mdelay(500);
+			AVR32_GPIO.port[LED3_PORT].ovrt = LED3_BIT_VALUE;
+			AVR32_GPIO.port[LED2_PORT].ovrt = LED2_BIT_VALUE;
+			mdelay(500);
+			AVR32_GPIO.port[LED2_PORT].ovrt = LED2_BIT_VALUE;
+			AVR32_GPIO.port[LED1_PORT].ovrt = LED1_BIT_VALUE;
+			mdelay(500);
+			AVR32_GPIO.port[LED1_PORT].ovrt = LED1_BIT_VALUE;
+			AVR32_GPIO.port[LED0_PORT].ovrt = LED0_BIT_VALUE;
+			mdelay(500);
+			AVR32_GPIO.port[LED0_PORT].ovrt = LED0_BIT_VALUE;
+			count = 0;
+		}
 	}
+	while(1);
 }
