d7c1b4172f499e09fd160a4fab08d3b5062567b9
bengtssondaniel <34157081+bengtssondaniel@users.noreply.github.com>
2018-09-14 11:38:07 +0200

    2

--- Lab_2/Lab2/Lab2/src/main.c
+++ Lab_2/Lab2/Lab2/src/main.c
@@ -2,6 +2,17 @@
 #include "board.h"
 #include "functions.h"
 
+//Define number of pins
+#define USART_MAX_PIN_NUMBER 31
+
+#define USART_PORT ( USART_RXD_PIN  / USART_MAX_PIN_NUMBER )
+#define RX_PIN USART_RXD_PIN
+#define RX_BIT_VALUE (1 << RX_PIN )
+
+#define USART_PORT ( USART_TXD_PIN  / USART_MAX_PIN_NUMBER )
+#define TX_PIN USART_TXD_PIN
+#define TX_BIT_VALUE (1 << TX_PIN )
+
 int main(void)
 {
 	volatile avr32_usart_t *usart = &AVR32_USART1;
