b641128921f78957af23750cd1e9d528800a2657
Wicktorl <wicktor90@hotmail.com>
2018-09-26 15:49:29 +0200

    Lab4_2

--- Lab_4_2/Lab_4_2/src/defines.h
+++ Lab_4_2/Lab_4_2/src/defines.h
@@ -17,42 +17,23 @@ typedef unsigned char uint8_t;
 #define GPIO_PORT1_ADDRESS ( GPIO_BASE_ADDRESS + GPIO_PORT1_OFFSET )
 
 /* Relative register offsets */
-#define GPIO_GPER_OFFSET 0x00
 #define GPIO_GPERS_OFFSET 0x04
-#define GPIO_GPERC_OFFSET 0x08
-#define GPIO_GPERT_OFFSET 0x0C
-
-#define GPIO_ODER_OFFSET 0x40
 #define GPIO_ODERS_OFFSET 0x44
-#define GPIO_ODERC_OFFSET 0x48
-#define GPIO_ODERT_OFFSET 0x4C
-
-#define GPIO_OVR_OFFSET 0x50
 #define GPIO_OVRS_OFFSET 0x54
 #define GPIO_OVRC_OFFSET 0x58
-#define GPIO_OVRT_OFFSET 0x5C
 
 /* Some defines so we can access the GPIO port 1 registers directly */
-#define GPIO_PORT1_GPER (*((volatile uint32_t*)(GPIO_PORT1_ADDRESS + GPIO_GPER_OFFSET)))
 #define GPIO_PORT1_GPERS (*((volatile uint32_t*)(GPIO_PORT1_ADDRESS + GPIO_GPERS_OFFSET)))
-#define GPIO_PORT1_GPERC (*((volatile uint32_t*)(GPIO_PORT1_ADDRESS + GPIO_GPERC_OFFSET)))
-#define GPIO_PORT1_GPERT (*((volatile uint32_t*)(GPIO_PORT1_ADDRESS + GPIO_GPERT_OFFSET)))
-
-#define GPIO_PORT1_ODER (*((volatile uint32_t*)(GPIO_PORT1_ADDRESS + GPIO_ODER_OFFSET)))
 #define GPIO_PORT1_ODERS (*((volatile uint32_t*)(GPIO_PORT1_ADDRESS + GPIO_ODERS_OFFSET)))
-#define GPIO_PORT1_ODERC (*((volatile uint32_t*)(GPIO_PORT1_ADDRESS + GPIO_ODERC_OFFSET)))
-#define GPIO_PORT1_ODERT (*((volatile uint32_t*)(GPIO_PORT1_ADDRESS + GPIO_ODERT_OFFSET)))
-
-#define GPIO_PORT1_OVR (*((volatile uint32_t*)(GPIO_PORT1_ADDRESS + GPIO_OVR_OFFSET)))
 #define GPIO_PORT1_OVRS (*((volatile uint32_t*)(GPIO_PORT1_ADDRESS + GPIO_OVRS_OFFSET)))
 #define GPIO_PORT1_OVRC (*((volatile uint32_t*)(GPIO_PORT1_ADDRESS + GPIO_OVRC_OFFSET)))
-#define GPIO_PORT1_OVRT (*((volatile uint32_t*)(GPIO_PORT1_ADDRESS + GPIO_OVRT_OFFSET)))
 
 /* --------------------------- Power Manager ------------------------   */
 
 /* Absolute PM BASE ADDRESS */
 #define PM_BASE_ADRESS 0xFFFF0C00
 
+/* PM register offsets */
 #define PM_MCCTRL_OFFSET 0x0000 //Main clock control
 #define PM_OSCCTRL0_OFFSET 0x0028 //Oscillator 0 control
 
@@ -61,10 +42,14 @@ typedef unsigned char uint8_t;
 
 /* ----------------------- Timer/Counter ----------------------- */
 
+/* TC base adress */
 #define TC_BASE_ADRESS 0xFFFF3800
+
+/* TC channel 0 offset */
 #define TC_CHANNEL_OFFSET 0x00
 #define TC_CHANNEL_ADRESS (TC_BASE_ADRESS + TC_CHANNEL_OFFSET)
 
+/* TC register offsets */
 #define TC_CCR_OFFSET 0x00
 #define TC_CMR_OFFSET 0x04
 #define TC_CV_OFFSET 0x10
--- Lab_4_2/Lab_4_2/src/functions.c
+++ Lab_4_2/Lab_4_2/src/functions.c
@@ -18,22 +18,25 @@ void LED0_output(uint8_t value)
 	if(value != 0)
 	{
 		/* Set the value on the LED0 pin to HIGH */
-		GPIO_PORT1_OVRS = (0x01 << 27);
+		GPIO_PORT1_OVRC = (0x01 << 27);
 	}
 	else
 	{
 	/* Set the value on the LED0 pin to LOW */
-	GPIO_PORT1_OVRC = (0x01 << 27);
+	GPIO_PORT1_OVRS = (0x01 << 27);
 	}
 }
 
 void CLK_init(void)
 {
+	/* Oscillator 0 is the source for the main clock, it initialized in PM with startup time 142 ms */
 	PM_MCCTRL = 0x05;
 	PM_OSCCTRL0 = 0x604;
 }
 void TC_init(void)
 {
+	/* Waveform options, fPBA / 8 is the clock frequency */
+	
 	TC_CCR = 0x05;
 	TC_CMR = 0x4002;
 }
