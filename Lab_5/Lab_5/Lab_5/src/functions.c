618035f24b981d9cedf5e195d08844cb59d3fc67
Wicktorl <wicktor90@hotmail.com>
2018-10-01 13:24:07 +0200

    Lab5_1

--- Lab_5/Lab_5/Lab_5/src/functions.c
+++ Lab_5/Lab_5/Lab_5/src/functions.c
@@ -21,7 +21,6 @@ void USART_reset(volatile avr32_usart_t *usart)
 	usart->CR.txen = 1;    //Enables the transmitter (1)
 
 }
-
 void USART_init(volatile avr32_usart_t * usart)
 {
 	
@@ -104,18 +103,6 @@ void USART_init(volatile avr32_usart_t * usart)
 	usart_PM->MCCTRL.osc1en = 0;    //Oscillator 1 DISABLED
 	usart_PM->MCCTRL.mcsel = 1;     //Oscillator 0 is the source for the main clock
 }
-
-char USART_pollChar(void)
-{
-	//Instead of waiting for a char, just looks once if
-	//there is something there and read it, else return nothing
-	volatile avr32_usart_t *usart = &AVR32_USART1;
-	
-	if(usart->CSR.rxrdy)
-	return usart->RHR.rxchr;
-	else
-	return;
-}
 void USART_putStr(char *c)
 {
 	volatile avr32_usart_t *usart = &AVR32_USART1;
@@ -129,6 +116,171 @@ void USART_putStr(char *c)
 		if(c[i] == '\0'){
 			usart->THR.txchr = '\n';
 			break;
+		}
+	}
+}
+void init_LED(void)
+{
+	volatile avr32_gpio_port_t * led0_port = &AVR32_GPIO.port[LED0_PORT];
+	led0_port->gpers = LED0_BIT_VALUE; //Set Enable register
+	led0_port->ovrs = LED0_BIT_VALUE; //Set output value register
+	led0_port->oders = LED0_BIT_VALUE; //Set output drive register
+	
+	volatile avr32_gpio_port_t * led1_port = &AVR32_GPIO.port[LED1_PORT];
+	led1_port->gpers = LED1_BIT_VALUE; //Set Enable register
+	led1_port->ovrs = LED1_BIT_VALUE; //Set output value register
+	led1_port->oders = LED1_BIT_VALUE; //Set output drive register
+	
+	volatile avr32_gpio_port_t * led2_port = &AVR32_GPIO.port[LED2_PORT];
+	led2_port->gpers = LED2_BIT_VALUE; //Set Enable register
+	led2_port->ovrs = LED2_BIT_VALUE; //Set output value register
+	led2_port->oders = LED2_BIT_VALUE; //Set output drive register
+}
+void vLED_TASK0(void* pvParameters)
+{
+	char c[32] = "LED 0 toggled";
+	volatile avr32_gpio_port_t * led0 = &AVR32_GPIO.port[LED0_PORT];
+	portTickType xLastWakeTime;
+	const portTickType xFrequency = 1000;
+
+	while(1)
+	{
+		xLastWakeTime = xTaskGetTickCount();
+		led0->ovrt = LED0_BIT_VALUE;
+		USART_putStr(c);
+		xLastWakeTime = xTaskGetTickCount();
+		vTaskDelayUntil(&xLastWakeTime, xFrequency);
+		led0->ovrt = LED0_BIT_VALUE;
+		USART_putStr(c);
+		xLastWakeTime = xTaskGetTickCount();
+		vTaskDelayUntil(&xLastWakeTime, xFrequency);
+	}
+}
+void vLED_TASK1(void* pvParameters)
+{
+	char c[32] = "LED 1 toggled";
+	volatile avr32_gpio_port_t * led1 = &AVR32_GPIO.port[LED1_PORT];
+	portTickType xLastWakeTime;
+	const portTickType xFrequency = 2000;
+
+	while(1)
+	{
+		
+		led1->ovrt = LED1_BIT_VALUE;
+		USART_putStr(c);
+		xLastWakeTime = xTaskGetTickCount();
+		vTaskDelayUntil(&xLastWakeTime, xFrequency);
+		
+		led1->ovrt = LED1_BIT_VALUE;
+		USART_putStr(c);
+		xLastWakeTime = xTaskGetTickCount();
+		vTaskDelayUntil(&xLastWakeTime, xFrequency);
+	}
+}
+void vLED_TASK2(void* pvParameters)
+{
+	char c[32] = "LED 2 toggled";
+	volatile avr32_gpio_port_t * led2 = &AVR32_GPIO.port[LED2_PORT];
+	portTickType xLastWakeTime;
+	const portTickType xFrequency = 3000;
+
+	while(1)
+	{
+		xLastWakeTime = xTaskGetTickCount();
+		led2->ovrt = LED2_BIT_VALUE;
+		USART_putStr(c);
+		xLastWakeTime = xTaskGetTickCount();
+		vTaskDelayUntil(&xLastWakeTime, xFrequency);
+		led2->ovrt = LED2_BIT_VALUE;
+		USART_putStr(c);
+		xLastWakeTime = xTaskGetTickCount();
+		vTaskDelayUntil(&xLastWakeTime, xFrequency);
+	}
+}
+void vbutton_TASK0(void* pvParameters)
+{
+	char c[32] = "Button 0 pressed";
+	char c1[32] = "LED 0 toggled";
+	volatile int button_state0; //Initialize button state 0
+	volatile avr32_gpio_port_t * led0 = &AVR32_GPIO.port[LED0_PORT];
+	portTickType xLastWakeTime;
+	const portTickType xFrequency = 10000;
+	xTaskHandle task  = *(xTaskHandle *)pvParameters;
+	
+	while(1){
+		
+		while(button_state0)
+		button_state0 = AVR32_GPIO.port[BUTTON_PORT0].pvr & BUTTON_PIN0;
+		
+		if(!button_state0){
+			USART_putStr(c);
+			vTaskSuspend(task);
+			led0->ovrc = LED0_BIT_VALUE;
+			USART_putStr(c1);
+			xLastWakeTime = xTaskGetTickCount();
+			vTaskDelayUntil(&xLastWakeTime, xFrequency);
+			vTaskResume(task);
+			
+			while(!button_state0)
+			button_state0 = AVR32_GPIO.port[BUTTON_PORT0].pvr & BUTTON_PIN0; //Read Button 1
+		}
+	}
+}
+void vbutton_TASK1(void* pvParameters)
+{
+	char c[32] = "Button 1 pressed";
+	char c1[32] = "LED 1 toggled";
+	volatile int button_state1; //Initialize button state 1
+	volatile avr32_gpio_port_t * led1 = &AVR32_GPIO.port[LED1_PORT];
+	portTickType xLastWakeTime;
+	const portTickType xFrequency = 10000;
+	xTaskHandle task  = *(xTaskHandle *)pvParameters;
+	
+	while(1){
+		
+		while(button_state1)
+		button_state1 = AVR32_GPIO.port[BUTTON_PORT1].pvr & BUTTON_PIN1;
+		
+		if(!button_state1){
+			USART_putStr(c);
+			vTaskSuspend(task);
+			led1->ovrc = LED1_BIT_VALUE;
+			USART_putStr(c1);
+			xLastWakeTime = xTaskGetTickCount();
+			vTaskDelayUntil(&xLastWakeTime, xFrequency);
+			vTaskResume(task);
+			
+			while(!button_state1)
+			button_state1 = AVR32_GPIO.port[BUTTON_PORT1].pvr & BUTTON_PIN1; //Read Button 1
+		}
+	}
+}
+void vbutton_TASK2(void* pvParameters)
+{
+	char c[32] = "Button 2 pressed";
+	char c1[32] = "LED 2 toggled";
+	volatile int button_state2; //Initialize button state 2
+	volatile avr32_gpio_port_t * led2 = &AVR32_GPIO.port[LED2_PORT];
+	portTickType xLastWakeTime;
+	const portTickType xFrequency = 10000;
+	xTaskHandle task = *(xTaskHandle *)pvParameters;
+
+	while(1){
+		
+		while(button_state2)
+		button_state2 = AVR32_GPIO.port[BUTTON_PORT2].pvr & BUTTON_PIN2; //Read Button 2
+		
+		if(!button_state2){
+			USART_putStr(c);
+			vTaskSuspend(task);
+			led2->ovrc = LED2_BIT_VALUE;
+			USART_putStr(c1);
+			xLastWakeTime = xTaskGetTickCount();
+			vTaskDelayUntil(&xLastWakeTime, xFrequency);
+			vTaskResume(task);
+			
+			while(!button_state2)
+			button_state2 = AVR32_GPIO.port[BUTTON_PORT2].pvr & BUTTON_PIN2; //Read Button 2
 		}
 	}
 }
--- Lab_5/Lab_5/Lab_5/src/functions.h
+++ Lab_5/Lab_5/Lab_5/src/functions.h
@@ -1,12 +1,51 @@
 #include "avr32\usart_400.h"
 #include "avr32\uc3a0512.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "board.h"
+#include "compiler.h"
 
 #ifndef FUNCTIONS_H_
 #define FUNCTIONS_H_
 
+#define GPIO_MAX_PIN_NUMBER 32
+
+//Define LED 0
+#define LED0_PORT ( LED0_GPIO / GPIO_MAX_PIN_NUMBER )
+#define LED0_PIN ( LED0_GPIO & ( GPIO_MAX_PIN_NUMBER -1))
+#define LED0_BIT_VALUE (1 << LED0_PIN )
+
+//Define LED 1
+#define LED1_PORT ( LED1_GPIO / GPIO_MAX_PIN_NUMBER )
+#define LED1_PIN ( LED1_GPIO & ( GPIO_MAX_PIN_NUMBER -1))
+#define LED1_BIT_VALUE (1 << LED1_PIN )
+
+//Define LED 2
+#define LED2_PORT ( LED2_GPIO / GPIO_MAX_PIN_NUMBER )
+#define LED2_PIN ( LED2_GPIO & ( GPIO_MAX_PIN_NUMBER -1))
+#define LED2_BIT_VALUE (1 << LED2_PIN )
+
+//Define button 0
+#define BUTTON_PORT0 (GPIO_PUSH_BUTTON_0 >> 5)
+#define BUTTON_PIN0 (1 << (GPIO_PUSH_BUTTON_0 & 0x1f))
+
+//Define button 1
+#define BUTTON_PORT1 (GPIO_PUSH_BUTTON_1 >> 5)
+#define BUTTON_PIN1 (1 << (GPIO_PUSH_BUTTON_1 & 0x1f))
+
+//Define button 1
+#define BUTTON_PORT2 (GPIO_PUSH_BUTTON_2 >> 5)
+#define BUTTON_PIN2 (1 << (GPIO_PUSH_BUTTON_2 & 0x1f))
+
 void USART_init(volatile avr32_usart_t * usart);
-char USART_pollChar();
+void init_LED(void);
 void USART_putStr(char *c);
 void USART_reset(volatile avr32_usart_t * usart);
+void vLED_TASK0(void* pvParameters);
+void vLED_TASK1(void* pvParameters);
+void vLED_TASK2(void* pvParameters);
+void vbutton_TASK0(void* pvParameters);
+void vbutton_TASK1(void* pvParameters);
+void vbutton_TASK2(void* pvParameters);
 
 #endif /* FUNCTIONS_H_ */
