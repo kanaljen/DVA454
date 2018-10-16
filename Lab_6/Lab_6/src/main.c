deadbee928ad87e6b0693d7a15da690179052be0
bengtssondaniel <34157081+bengtssondaniel@users.noreply.github.com>
2018-10-16 14:38:07 +0200

    lab6

--- Lab_6/Lab_6/src/main.c
+++ Lab_6/Lab_6/src/main.c
@@ -4,9 +4,10 @@ int main(void)
 {	
 	xTaskHandle *ProducerTaskHandle = malloc(sizeof(xTaskHandle*));
 	xTaskHandle *ConsumerTaskHandle = malloc(sizeof(xTaskHandle*));
-	
+	xSemaphoreHandle *xSem = malloc(sizeof(xSemaphoreHandle*));
 	USART_init();
 	
+	xTaskCreate(vSemaphoreTask, "xsemTask0", 256, NULL, tskIDLE_PRIORITY+1, NULL);
 	xTaskCreate(vProducerTask, "xTask0", 256, &ConsumerTaskHandle, tskIDLE_PRIORITY, &ProducerTaskHandle);
 	xTaskCreate(vConsumerTask, "xTask1", 256, &ProducerTaskHandle, tskIDLE_PRIORITY, &ConsumerTaskHandle);
 	usart_write_line(configDBG_USART , "Starting Scheduler\n");
