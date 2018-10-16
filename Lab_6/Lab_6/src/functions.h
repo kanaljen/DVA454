807c29e1603f454374a9d82ef4c3eb814312875e
bengtssondaniel <34157081+bengtssondaniel@users.noreply.github.com>
2018-10-16 11:01:45 +0200

    lab6

--- Lab_6/Lab_6/src/functions.h
+++ Lab_6/Lab_6/src/functions.h
@@ -17,33 +17,6 @@
 #define FALSE 0
 #define GPIO_MAX_PIN_NUMBER 32
 
-//Define LED 0
-#define LED0_PORT ( LED0_GPIO / GPIO_MAX_PIN_NUMBER )
-#define LED0_PIN ( LED0_GPIO & ( GPIO_MAX_PIN_NUMBER -1))
-#define LED0_BIT_VALUE (1 << LED0_PIN )
-
-//Define LED 1
-#define LED1_PORT ( LED1_GPIO / GPIO_MAX_PIN_NUMBER )
-#define LED1_PIN ( LED1_GPIO & ( GPIO_MAX_PIN_NUMBER -1))
-#define LED1_BIT_VALUE (1 << LED1_PIN )
-
-//Define LED 2
-#define LED2_PORT ( LED2_GPIO / GPIO_MAX_PIN_NUMBER )
-#define LED2_PIN ( LED2_GPIO & ( GPIO_MAX_PIN_NUMBER -1))
-#define LED2_BIT_VALUE (1 << LED2_PIN )
-
-//Define button 0
-#define BUTTON_PORT0 (GPIO_PUSH_BUTTON_0 >> 5)
-#define BUTTON_PIN0 (1 << (GPIO_PUSH_BUTTON_0 & 0x1f))
-
-//Define button 1
-#define BUTTON_PORT1 (GPIO_PUSH_BUTTON_1 >> 5)
-#define BUTTON_PIN1 (1 << (GPIO_PUSH_BUTTON_1 & 0x1f))
-
-//Define button 1
-#define BUTTON_PORT2 (GPIO_PUSH_BUTTON_2 >> 5)
-#define BUTTON_PIN2 (1 << (GPIO_PUSH_BUTTON_2 & 0x1f))
-
 #define configDBG 1
 #define configDBG_USART (&AVR32_USART1)
 #define configDBG_USART_RX_PIN AVR32_USART1_RXD_0_0_PIN
@@ -61,12 +34,7 @@
 #define serialPORT_USART_BAUDRATE 9600
 
 void USART_init(void);
-void init_LED(void);
 void vLED_TASK0(void* pvParameters);
 void vLED_TASK1(void* pvParameters);
-void vLED_TASK2(void* pvParameters);
-void vbutton_TASK0(void* pvParameters);
-void vbutton_TASK1(void* pvParameters);
-void vbutton_TASK2(void* pvParameters);
 
 #endif /* FUNCTIONS_H_ */
--- Lab_6/Lab_6/src/main.c
+++ Lab_6/Lab_6/src/main.c
@@ -6,13 +6,11 @@ int main(void)
 	xSemaphoreHandle *xSem = malloc(sizeof(xSemaphoreHandle*));
 	
 	USART_init();
-	init_LED();
 	
 	vSemaphoreCreateBinary(xSem);
 	
-	xTaskCreate(vLED_TASK0, "LED0", 256, &xSem, tskIDLE_PRIORITY+3, NULL);
-	xTaskCreate(vLED_TASK1, "LED1", 256, &xSem, tskIDLE_PRIORITY+2, NULL);
-	xTaskCreate(vLED_TASK2, "LED2", 256, &xSem, tskIDLE_PRIORITY+1, NULL);
+	xTaskCreate(vLED_TASK0, "LED0", 256, &xSem, tskIDLE_PRIORITY, NULL);
+	xTaskCreate(vLED_TASK1, "LED1", 256, &xSem, tskIDLE_PRIORITY, NULL);
 	usart_write_line(configDBG_USART , "Starting Scheduler\n");
 	vTaskStartScheduler();
 	while(1);
